0696a0fa69b24c0e75f423975fff9fc3d9fea594
Marykeje <marykeje01@gmail.com>
2024-04-08 14:42:33 +0100

    made some corrections

--- js/script.js
+++ js/script.js
@@ -1,6 +1,39 @@
+
 const navBtnEl = document.querySelector(".btn-nav-mobile");
 const headerEl = document.querySelector(".header");
 
 navBtnEl.addEventListener("click", function () {
   headerEl.classList.toggle("open-nav");
 });
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    // to scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behaviour: "smooth",
+      });
+  });
+});
+
+// Sticky Navigation
+const sectionHeroEl = document.querySelector(".waste-body");
+
+const obs = new IntersectionObserver(function (entries) {
+  const ent = entries[0];
+  console.log(ent);
+
+  if (ent.isIntersecting === false) {
+    document.body.classList.add("sticky");
+  }
+
+  if (ent.isIntersecting === true) {
+    document.body.classList.remove("sticky");
+  }
+});
+obs.observe(sectionHeroEl);
